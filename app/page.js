ec0a833173b578960adfc8af6621ff9bb58abe2f
kishansuthar000 <12kishan97@gmail.com>
2024-10-18 13:04:32 +0000

    uat

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Page() {
   return (
     <>
-      <h1>This is from uat</h1>
+      <h1>This is from uat -- </h1>
       <Link href={"/home"}>Home</Link>
     </>
   );
